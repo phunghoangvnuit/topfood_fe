8850fa1d0d8f6be19c0e82f680408cfdf682d308
phunghoangvnuit <phunghoangvnuit@gmail.com>
2024-11-18 08:17:11 +0700

    update cart page - not finished yet

---
+++ src/component/Cart/Cart.jsx
@@ -0,0 +1,84 @@
+import { Button, Card, Divider, Modal } from "@mui/material";
+import React from "react";
+import { CartItem } from "./CartItem";
+import { AddressCard } from "./AddressCard";
+import AddLocationAltIcon from "@mui/icons-material/AddLocationAlt";
+import { AddLocation } from "@mui/icons-material";
+
+const items = [1, 1];
+const Cart = () => {
+  const createOrderUsingSelectedAddress = () => {};
+  const handleOpenAddressModal = () => {};
+  return (
+    <>
+      <main className="lg:flex justify-between">
+        <section className="lg:w-[30%] space-y-6 lg:min-h-screen pt-10">
+          {items.map((item) => (
+            <CartItem />
+          ))}
+          <Divider />
+          <div className="billDetails px-5 text-sm">
+            <p className="font-extralight py-5">Bill Details</p>
+            <div className="space-y-3">
+              <div className="flex justify-between text-gray-400">
+                <p>Item Total</p>
+                <p>60.000 VND</p>
+              </div>
+              <div className="flex justify-between text-gray-400">
+                <p>Delivery Fee</p>
+                <p>20.000 VND</p>
+              </div>
+              <div className="flex justify-between text-gray-400">
+                <p>Tax (VAT)</p>
+                <p>10.000 VND</p>
+              </div>
+              <Divider />
+            </div>
+            <div className="flex justify-between text-gray-400">
+              <p>Total pay</p>
+              <p>90.000 VND</p>
+            </div>
+          </div>
+        </section>
+        <Divider orientation="vertical" flexItem />
+        <section className="lg:w-[70%] flex justify-center px-5 pb-10 lg:pb-0">
+          <div>
+            <h1 className="text-center font-semibold text-2xl py-10">
+              Choose Delivery Address
+            </h1>
+            <div className="flex gap-5 flex-wrap justify-center">
+              {[1, 1, 1, 1, 1].map((item) => (
+                <AddressCard
+                  handleSelectAddress={createOrderUsingSelectedAddress}
+                  item={item}
+                  showButton={true}
+                />
+              ))}
+              <Card className="flex gap-5 w-64 p-5">
+                <AddLocation />
+                <div className="space-y-3 text-gray-500">
+                  <h1 className="font-semibold text-lg text-white">
+                    Add New Address
+                  </h1>
+
+                  <Button
+                    variant="outlined"
+                    fullWidth
+                    onClick={handleOpenAddressModal}
+                  >
+                    Add
+                  </Button>
+                </div>
+              </Card>
+            </div>
+          </div>
+        </section>
+      </main>
+      {/* <Modal> - continue
+
+      </Modal> */}
+    </>
+  );
+};
+
+export default Cart;
