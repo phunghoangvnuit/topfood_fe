6da8137c9312bc90b0362f4448af586feaadd4fd
phunghoangvnuit <phunghoangvnuit@gmail.com>
2024-11-28 21:05:40 +0700

    finish create form for food category

--- src/AdminComponent/FoodCategory/CreateFoodCategoryForm.jsx
+++ src/AdminComponent/FoodCategory/CreateFoodCategoryForm.jsx
@@ -1,9 +1,51 @@
-import React from 'react'
+import { Button, TextField } from "@mui/material";
+import React, { useState } from "react";
 
 const CreateFoodCategoryForm = () => {
+  const [formData, setFormData] = useState({
+    categoryName: "",
+    resturantId: "",
+  });
+  const handleSubmit = () => {
+    const data = {
+      name: formData.categoryName,
+      restaurantId: {
+        id: 1,
+      },
+    };
+    console.log(data);
+  };
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
   return (
-    <div>CreateFoodCategoryForm</div>
-  )
-}
+    <div className="">
+      <div className="p-5">
+        <h1 className="text-gray-400 text-center text-xl pb-10">
+          Create Category
+        </h1>
+        <form className="space-y-5" onSubmit={handleSubmit}>
+          <TextField
+            fullWidth
+            id="categoryName"
+            name="categoryName"
+            label="Category Name"
+            variant="outlined"
+            onChange={handleInputChange}
+            value={formData.categoryName}
+          ></TextField>
 
-export default CreateFoodCategoryForm
+          <Button variant="contained" type="submit">
+            Create Category
+          </Button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default CreateFoodCategoryForm;
