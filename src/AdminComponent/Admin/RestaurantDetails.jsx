134634318b52ab822a0b42f7914b79e472868ad3
phunghoangvnuit <phunghoangvnuit@gmail.com>
2024-11-29 11:41:41 +0700

    fe to be 004 - restaurant details page

--- src/AdminComponent/Admin/RestaurantDetails.jsx
+++ src/AdminComponent/Admin/RestaurantDetails.jsx
@@ -4,24 +4,35 @@ import InstagramIcon from '@mui/icons-material/Instagram';
 import TwitterIcon from '@mui/icons-material/Twitter';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import FacebookIcon from '@mui/icons-material/Facebook';
+import { useDispatch, useSelector } from "react-redux";
+import { updateRestaurantStatus } from "component/State/Restaurant/Action";
 
 export const RestaurantDetails = () => {
-  const handleRestaurantStatus = () => {};
+  const { restaurant } = useSelector((store) => store);
+  const dispatch = useDispatch();
+
+  console.log("Restaurant Details",restaurant);
+  const handleRestaurantStatus = () => {
+    dispatch(updateRestaurantStatus({
+      restaurantId: restaurant.usersRestaurant.id,
+      jwt: localStorage.getItem("jwt")
+    }))
+  };
   return (
     <div className="lg:px-20 px-5 pb-10">
       <div className="py-5 flex justify-center items-center gap-5">
         <h1 className="text-2xl lg:text-7xl text-center font-bold p-5">
-          Banh Mi Viet Nam
+          {restaurant.usersRestaurant?.name}
         </h1>
         <div>
           <Button
-            color={true ? "primary" : "error"}
+            color={!restaurant.usersRestaurant?.open ? "primary" : "error"}
             className="py-[1rem] px-[2rem]"
             variant="contained"
             onClick={handleRestaurantStatus}
             size="large"
           >
-            {true ? "close" : "open"}
+            {restaurant.usersRestaurant?.open ? "close" : "open"}
           </Button>
         </div>
       </div>
@@ -37,35 +48,35 @@ export const RestaurantDetails = () => {
                   <p className="w-48">Owner</p>
                   <p className="text-gray-400">
                     <span className="pr-5">-</span>
-                    Phung Hoang
+                    {restaurant.usersRestaurant?.owner.fullName}
                   </p>
                 </div>
                 <div className="flex">
                   <p className="w-48">Restaurant Name</p>
                   <p className="text-gray-400">
                     <span className="pr-5">-</span>
-                    Phung Hoang
+                    {restaurant.usersRestaurant?.name}
                   </p>
                 </div>
                 <div className="flex">
                   <p className="w-48">Cusine Type</p>
                   <p className="text-gray-400">
                     <span className="pr-5">-</span>
-                    Phung Hoang
+                    {restaurant.usersRestaurant?.cuisineType}
                   </p>
                 </div>
                 <div className="flex">
                   <p className="w-48">Opening Hours</p>
                   <p className="text-gray-400">
                     <span className="pr-5">-</span>
-                    Phung Hoang
+                    {restaurant.usersRestaurant?.openingHours}
                   </p>
                 </div>
                 <div className="flex">
                   <p className="w-48">Status</p>
                   <p className="text-gray-400">
                     <span className="pr-5">-</span>
-                    {true?<span className="px-5 py-2 rounded-full bg-green-400 text-gray-950">Open</span>:<span className="px-5 py-2 rounded-full bg-red-400 text-gray-950">Closed</span>}
+                    {restaurant.usersRestaurant?.open?<span className="px-5 py-2 rounded-full bg-green-400 text-gray-950">Open</span>:<span className="px-5 py-2 rounded-full bg-red-400 text-gray-950">Closed</span>}
                   </p>
                 </div>
               </div>
@@ -123,14 +134,14 @@ export const RestaurantDetails = () => {
                   <p className="w-48">Email</p>
                   <p className="text-gray-400">
                     <span className="pr-5">-</span>
-                    Phung Hoang
+                    {restaurant.usersRestaurant?.contactInformation?.email}
                   </p>
                 </div>
                 <div className="flex">
                   <p className="w-48">Mobile</p>
                   <p className="text-gray-400">
                     <span className="pr-5">-</span>
-                    Phung Hoang
+                    {restaurant.usersRestaurant?.contactInformation?.mobile}
                   </p>
                 </div>
 
@@ -138,10 +149,10 @@ export const RestaurantDetails = () => {
                   <p className="w-48">Social</p>
                   <div className="flex text-gray-400 items-center pb-3 gap-2">
                     <span className="pr-5">-</span>
-                    <a href="/">
+                    <a href={restaurant.usersRestaurant?.contactInformation?.instagram}>
                       <InstagramIcon sx={{fontSize:"3rem"}}/>
                     </a>
-                    <a href="/">
+                    <a href={restaurant.usersRestaurant?.contactInformation?.twitter}>
                       <TwitterIcon sx={{fontSize:"3rem"}}/>
                     </a>
                     <a href="/">
