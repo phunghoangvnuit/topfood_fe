af658c63bb71340cd83648cf3cca28e3cb7e82ac
phunghoangvnuit <phunghoangvnuit@gmail.com>
2024-11-29 11:15:21 +0700

    fe to be 004 - create restaurant form

--- src/Routers/AdminRoute.jsx
+++ src/Routers/AdminRoute.jsx
@@ -1,16 +1,21 @@
-import { Admin } from 'AdminComponent/Admin/Admin';
-import CreateRestaurantForm from 'AdminComponent/CreateRestaurantForm/CreateRestaurantForm';
-import React from 'react';
-import { Route, Routes } from 'react-router-dom';
+import { Admin } from "AdminComponent/Admin/Admin";
+import CreateRestaurantForm from "AdminComponent/CreateRestaurantForm/CreateRestaurantForm";
+import React from "react";
+import { useSelector } from "react-redux";
+import { Route, Routes } from "react-router-dom";
 
 export const AdminRoute = () => {
+  const { restaurant } = useSelector((store) => store);
   return (
     <div>
       <Routes>
-        <Route path="/*" element={false?<CreateRestaurantForm/>:<Admin/>}>
-        
-        </Route>
+        <Route
+          path="/*"
+          element={
+            !restaurant.usersRestaurant ? <CreateRestaurantForm /> : <Admin />
+          }
+        ></Route>
       </Routes>
     </div>
-  )
-}
+  );
+};
