cb5fc0810f491d5fb48c05190f623680c099941b
phunghoangvnuit <phunghoangvnuit@gmail.com>
2025-02-02 16:27:34 +0700

    be & fe - 104 - update user sidebar ui

--- src/component/Profile/ProfileNavigation.jsx
+++ src/component/Profile/ProfileNavigation.jsx
@@ -22,40 +22,63 @@ const menu = [
   { title: "Logout", icon: <LogoutIcon /> },
 ];
 export const ProfileNavigation = ({ open, handleClose }) => {
-  const isSmallScreen = useMediaQuery('(max-width:900px)');
+  const isSmallScreen = useMediaQuery("(max-width:900px)");
   const navigate = useNavigate();
   const dispatch = useDispatch();
 
-  const handleNavigate=(item) => {
-    if(item.title==="Logout"){
+  const handleNavigate = (item) => {
+    if (item.title === "Logout") {
       dispatch(logout());
-      navigate("/")
-    }
-    else
-    navigate(`/my-profile/${item.title.toLowerCase()}`)
-  }
+      navigate("/");
+    } else navigate(`/my-profile/${item.title.toLowerCase()}`);
+  };
 
   return (
     <div>
-      <Drawer
-        variant={isSmallScreen ? "temporary" : "permanent"}
-        onClose={handleClose}
-        open={true}
-        anchor="left"
-        sx={{ zIndex: -1, position:"sticky" }}
-      >
-        <div className="w-[70vw] lg:w-[20vw] h-screen flex flex-col justify-center text-xl space-y-[1.65rem]" style={{backgroundColor: "#ED1C24"}}>
-          {menu.map((item, i) => (
-            <>
-              <div onClick={()=>handleNavigate(item)} className="px-5 flex items-center space-x-5 cursor-pointer">
-                {item.icon}
-                <span>{item.title}</span>
-              </div>
-              {i !== menu.length - 1 && <Divider sx={{bgcolor: "#D4D4D4", opacity:"0.5"}} />}
-            </>
-          ))}
-        </div>
-      </Drawer>
+      <>
+        <Drawer
+          variant={isSmallScreen ? "temporary" : "permanent"}
+          onClose={handleClose}
+          open={true}
+          anchor="left"
+          sx={{ zIndex: -1, position: "sticky" }}
+        >
+          <div
+            className="w-[70vw] lg:w-[20vw] h-screen flex flex-col text-xl"
+            style={{ backgroundColor: "#212529" }}
+          >
+            <div
+              className="w-[70vw] lg:w-[20vw] h-screen flex flex-col text-xl space-y-[1.65rem]"
+              style={{marginTop: "120px"}}
+            >
+              {menu.map((item, i) => (
+                <>
+                  <div
+                    onClick={() => handleNavigate(item)}
+                    className="px-5 flex items-center space-x-5 cursor-pointer"
+                    style={{color: "#FFFFFF"}}
+                  >
+                    {item.icon}
+                    <span>{item.title}</span>
+                  </div>
+                  {i !== menu.length - 1 && (
+                    <Divider sx={{ bgcolor: "#D4D4D4", opacity: "0.5" }} />
+                  )}
+                </>
+              ))}
+            </div>
+            <div
+              style={{ color: "gray", textAlign: "center", fontSize: "16px" }}
+            >
+              <p>
+                Foody.vn - v1.0.0 - 17.01.2025
+                <br />
+                @phunghoangvnuit - FPT Aptech
+              </p>
+            </div>
+          </div>
+        </Drawer>
+      </>
     </div>
   );
 };
